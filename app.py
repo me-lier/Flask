32f3128e36da9fd62d6359a1b2633169298353bb
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2024-03-15 14:25:18 +0000

    update of html file

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template as r, url_for, request, redirect
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def HelloWorld():
-  return "Hello World"
+  return r("index.html")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+  if request.method == "POST":
+    user = request.form['username']
+    return redirect(url_for("user", usr=user))
+  else:
+    return r('login.html')
+
+@app.route("/<usr>")
+def user(usr):
+  return f"hii, {usr}"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
